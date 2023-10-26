9154be6ad6310b4e5e9d9f39695cbd0b6359bc08
RemEx <74838147+Rem7474@users.noreply.github.com>
2023-10-26 11:49:05 +0000

    nouveau js

--- js/conso.js
+++ js/conso.js
@@ -1,7 +1,21 @@
-window.addEventListener("storage",check_conso_en_stock);
+//variables globales
+var Temp_Local_Storage={};
+window.addEventListener("storage",update_windows);
 
-function check_conso_en_stock(){
+function update_windows(){
     let stock = stock_actuel();
+    //liste des boutons de la page
+    let boutons=document.getElementsByClassName("conso");
+    for (let bouton of boutons){
+        let idconso=bouton.getAttribute("id");
+        if (stock[idconso]==undefined){
+            bouton.setAttribute("disabled","disabled");
+        }
+        else{
+            bouton.addEventListener("click",add_panier);
+            bouton.removeAttribute("disabled");
+        }
+    }
 }
 function stock_actuel(){
     let stock=localStorage.getItem("stock_courses")
@@ -21,96 +35,32 @@ function stock_actuel(){
             delete stock[conso];
         }
     }
-    console.log(stock);
     return stock;
 }
-
-function calcul_stock(details){
-    let consommations={};
-    let stock_restant={};
-    let stock=localStorage.getItem("stock_courses")
-    stock=JSON.parse(stock);
-    let conso_by_day={};
-    //récupère les données du local storage
-    let keys=Object.keys(localStorage);    
-    //stock actuel :
-    console.log("stock actuel :")
-    console.log(stock);
-    //calcul des consommations
-    for (let key of keys){
-        if (key!="stock_courses"){
-            let value=localStorage.getItem(key);
-            value=JSON.parse(value);
-            //récupère le dictionnaire des consos pour afficher les données
-            for(let conso in value.consos){
-                let quantite=parseInt(value.consos[conso]);
-                let prix=parseFloat(value.consos[conso])*stock[conso]["prix"];
-                if(conso_by_day[value.jour]==undefined){
-                    conso_by_day[value.jour]={};
-                    conso_by_day[value.jour]["quantité"]=quantite;
-                    conso_by_day[value.jour]["prix"]=prix;
-                }
-                else{
-                    conso_by_day[value.jour]["quantité"]+=quantite;
-                    conso_by_day[value.jour]["prix"]+=prix;
-                }
-                if(consommations[conso]==undefined){
-                    consommations[conso]={};
-                    consommations[conso]["quantité"]=quantite;
-                    consommations[conso]["prix"]=prix;
-                }
-                else{
-                    consommations[conso]["quantité"]+=quantite;
-                    consommations[conso]["prix"]+=prix;
-                }
-            }
-        }
+function add_panier(event){
+    //ajout de la consommation au panier a afficher (Affichage_Panier) et au panier temporaire (Temp_Local_Storage)
+    //si il est déjà présent on incrémente la quantité
+    //sinon on le rajoute
+    let idconso=event.target.getAttribute("id");
+    let nameConso=event.target.getAttribute("name");
+    let prixConso=parseFloat(event.target.getAttribute("value"));
+    if (Temp_Local_Storage[idconso]==undefined){
+        Temp_Local_Storage[idconso]={};
+        Temp_Local_Storage[idconso]["quantité"]=1;
+        Temp_Local_Storage[idconso]["prix"]=prixConso;
+        Temp_Local_Storage[idconso]["nom"]=nameConso;
     }
-
-
-    console.log("consommations :");
-    console.log(consommations);
-    console.log("consommations by day :");
-    console.log(conso_by_day);
-    
-    for (let consos in stock){
-        if(consommations[consos]==undefined){
-            stock_restant[consos]={};
-            stock_restant[consos]["quantité"]=stock[consos]["quantité"];
-            stock_restant[consos]["prix"]=stock[consos]["prix"]*stock[consos]["quantité"];
-        }
-        else{
-            stock_restant[consos]={};
-            stock_restant[consos]["quantité"]=stock[consos]["quantité"]-consommations[consos]["quantité"];
-            stock_restant[consos]["prix"]=stock_restant[consos]["quantité"]*stock[consos]["prix"];
-        }
-        if(stock_restant[consos]["quantité"]<=0){
-            //supprime la clé si le stock est vide
-            delete stock_restant[consos];
-        }
-
+    else{
+        Temp_Local_Storage[idconso]["quantité"]+=1;
+        Temp_Local_Storage[idconso]["prix"]+=prixConso;
+        
     }
-    console.log("stock restant :");
-    console.log(stock_restant);
-    switch(details){
-        case "consommations":
-            console.log("consommations");
-            return consommations;
-            break;
-        case "stock_restant":
-            console.log("stock restant");
-            return stock_restant;
-            break;
-        case "conso_by_day":
-            console.log("conso by day");
-            return conso_by_day;
-            break;
-        case "all":
-            console.log("all");
-            return [consommations,stock_restant,conso_by_day];
-            break;
-        default:
-            return stock_restant;
-            break;
+}
+function prix_panier(){
+    let prix=0;
+    //calcul du prix du panier temporaire, et déduit les reductions appliquables
+    for (let conso in Temp_Local_Storage){
+        prix+=Temp_Local_Storage[conso]["prix"];
     }
+    return prix;
 }
