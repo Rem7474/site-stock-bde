ae385ae58cb54c34bf23f85c2cafd363af7b062b
Rem7474 <recuv007@hotmail.fr>
2023-10-27 23:41:58 +0200

    modification js css

--- js/statvente.js
+++ js/statvente.js
@@ -1,9 +1,23 @@
 //AUTHOR: REMY CUVELIER
 //DATE: 2023.10.27
 //PROJECT: STOCK BDE
-window.addEventListener("load",update_windows);
+window.addEventListener("load",init_page);
 window.addEventListener("storage",update_windows);
-
+function init_page(){
+    let liste=document.getElementById("nb_jour");
+    //possibilité de choisir entre 1 et 30 jours
+    for(let i=1;i<=30;i++){
+        let option=document.createElement("option");
+        option.value=i;
+        option.innerHTML=i;
+        if (i==5){
+            option.selected="selected";
+        }
+        liste.appendChild(option);
+    }
+    liste.addEventListener("change",update_windows);
+    update_windows();
+}
 function update_windows(){
     //appel de la fonction de calcul des stocks
     let stock_courses = localStorage.getItem("stock_courses");
@@ -32,7 +46,10 @@ function update_windows(){
     console.log(prix_stock_restant_vente);
     console.log("infos des ventes par jour :");
     console.log(infos_ventes_jours);
-
+    //affichage des stocks
+    graphique(infos_ventes_jours,document.getElementById("nb_jour").value);
+    tableau_stock_restant(stock_restant,prix_stock_restant_vente);
+    update_html(prix_stock_restant_achat,prix_stock_restant_vente,prix_achat,prix_vente);
 }
 function get_prix_achat(stock_courses){
     let keys = Object.keys(stock_courses);
@@ -40,7 +57,7 @@ function get_prix_achat(stock_courses){
     let value;
     for (let key of keys){
         value = stock_courses[key];
-        prix_achat += parseFloat(value["prix_achat"])*parseInt(value["quantité"])*0.8;
+        prix_achat += parseFloat(value["prix_achat"])*parseInt(value["quantité"]);
     }
     return prix_achat.toFixed(2);
 }
@@ -51,7 +68,7 @@ function get_prix_vente(stock_courses){
     let value;
     for (let key of keys){
         value = stock_courses[key];
-        prix_vente += parseFloat(value["prix"])*parseInt(value["quantité"]);
+        prix_vente += parseFloat(value["prix"])*parseInt(value["quantité"])*0.8;
     }
     return prix_vente.toFixed(2);
 }
@@ -82,7 +99,7 @@ function get_prix_stock_restant_achat(stock_restant){
     let value;
     for (let key of keys){
         value = stock_restant[key];
-        prix_stock_restant_achat += parseFloat(value["prix_achat"])*parseInt(value["quantité"])*0.8;
+        prix_stock_restant_achat += parseFloat(value["prix_achat"])*parseInt(value["quantité"]);
     }
     return prix_stock_restant_achat.toFixed(2);
 }
@@ -93,7 +110,7 @@ function get_prix_stock_restant_vente(stock_restant){
     let value;
     for (let key of keys){
         value = stock_restant[key];
-        prix_stock_restant_vente += parseFloat(value["prix"])*parseInt(value["quantité"]);
+        prix_stock_restant_vente += parseFloat(value["prix"])*parseInt(value["quantité"])*0.8;
     }
     return prix_stock_restant_vente.toFixed(2);
 }
@@ -121,4 +138,128 @@ function get_infos_ventes_jours(){
         }
     }
     return infos_ventes_jours;
+}
+
+function graphique(data_day,nb_day){
+    //reset le canvas
+    document.getElementById("myChart").remove();
+    let canvas=document.createElement("canvas");
+    canvas.id="myChart";
+    document.getElementById("graphique").appendChild(canvas);
+    let labels=Object.keys(data_day);
+    //trie les labels par ordre décroissant
+    labels.sort().reverse();
+    let quantite=[];
+    let prix=[];
+    let day=[];
+    let date=new Date();
+    let jour=date.getDate()-nb_day;
+    let mois=date.getMonth()+1;
+    let annee=date.getFullYear();
+    let date_jour=jour+"/"+mois+"/"+annee;
+
+    for(let label of labels){
+        //check si le jour est dans la limite du nb_day
+        if(label>date_jour){
+            quantite.push(data_day[label]["quantité"]);
+            prix.push(data_day[label]["prix"]);
+            day.push(label);
+        }
+
+        
+
+    }
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: day,
+            datasets: [{
+                label: 'Quantité',
+                data: quantite,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor:'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            },
+            {
+                label: 'Prix',
+                data: prix,
+                backgroundColor: 'rgba(99, 255, 132, 0.2)',
+                borderColor:'rgba(99, 255, 132, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        // Include a dollar sign in the ticks
+                        callback: function(value, index, values) {
+                            return value + '€';
+                        }
+                    }
+                }
+            }
+        }
+    });
+}
+
+function tableau_stock_restant(stock,prix_vente){
+        //affichage du panier temporaire
+        let div=document.getElementById("stock_restant");
+        div.innerHTML="";
+        let panier=document.createElement("table");
+        panier.innerHTML="";
+        panier.className="table_stock";
+        let ligne=document.createElement("tr");
+        let cellule1=document.createElement("td");
+        cellule1.className="colonne1";
+        let cellule2=document.createElement("td");
+        cellule2.className="colonne2";
+        cellule1.innerHTML="Nom";
+        cellule2.innerHTML="Qté";
+        ligne.appendChild(cellule1);
+        ligne.appendChild(cellule2);
+        panier.appendChild(ligne);
+    
+        for (let conso in stock){
+            ligne=document.createElement("tr");
+            cellule1=document.createElement("td");
+            cellule1.className="colonne1";
+            cellule2=document.createElement("td");
+            cellule2.className="colonne2";
+            cellule1.innerHTML=conso;
+            cellule2.innerHTML=stock[conso]["quantité"];
+            ligne.appendChild(cellule1);
+            ligne.appendChild(cellule2);
+            panier.appendChild(ligne);
+        }
+        ligne=document.createElement("tr");
+        cellule1=document.createElement("td");
+        cellule2=document.createElement("td");
+        cellule1.innerHTML="Total";
+        cellule2.innerHTML=prix_vente+"€";
+        ligne.appendChild(cellule1);
+        ligne.appendChild(cellule2);
+        panier.appendChild(ligne);
+        div.appendChild(panier);
+}
+
+function update_html(prix_stock_restant_achat,prix_stock_restant_vente,prix_achat,prix_vente){
+    let marge_total=prix_vente-prix_achat;
+    let marge_actuel=prix_achat-prix_stock_restant_achat;
+    let prix_vendu=prix_vente-prix_stock_restant_vente;
+    let prix_achat_vendu=prix_achat-prix_stock_restant_achat;
+    let pourcentage_prix_vendu=(prix_vendu/prix_achat*100).toFixed(2);
+    let pourcetage_marge=(marge_total/prix_achat*100).toFixed(2);
+    let texte="Prix d'achat total : "+prix_achat+"€<br>";
+    texte+="Prix de vente total : "+prix_vente+"€<br>";
+    texte+="Marge total des courses: "+marge_total.toFixed(2)+"€<br>";
+    texte+="Prix d'achat du stock restant : "+prix_stock_restant_achat+"€<br>";
+    texte+="Prix de vente du stock restant : "+prix_stock_restant_vente+"€<br>";
+    texte+="Marge actuel : "+marge_actuel.toFixed(2)+"€<br>";
+    texte+="Pourcentage vendu : "+pourcentage_prix_vendu+"%<br>";
+    texte+="Pourcentage de marge moyenne : "+pourcetage_marge+"%<br>";
+    document.getElementById("prix").innerHTML=texte;
 }
